931c8cb90a5c4102cf7b2fbf5b93c824ee832d24
Felipe Ramos Ferreira <perenecabuto@gmail.com>
2013-05-07 15:18:16 -0300

    Adding gridster

---
+++ bmo_controller/static/bmo_controller/js/base.js
@@ -0,0 +1,50 @@
+$(function() {
+    $('.post-link').on('click', function(e) {
+        e.preventDefault();
+        $('<form action="' + this.href + '" method="POST" />').submit();
+    });
+
+    $('.ajax-link').on('mousedown', function(e) {
+        $(this).attr('last-click', new Date().getMilliseconds());
+    });
+
+    $('.ajax-link').live('click', function(e) {
+        var lastClick = $(this).attr('last-click'),
+            interval = Math.abs(new Date().getMilliseconds() - lastClick);
+
+        e.preventDefault();
+        $(this).attr('last-click', null);
+
+        if (interval <= 100) {
+            $.ajax({ url: this.href });
+        }
+
+        return false;
+    });
+
+    randCommandBtnColors();
+
+    $('[data-rand-cmd-colors]').on('click', function() {
+        randCommandBtnColors();
+        return false;
+    });
+
+    $('.nav li').each(function(idx, menuItem) {
+        var item = $(menuItem),
+        link = item.find('a:eq(0)'),
+        re = new RegExp('^.+://' + window.location.host);
+
+        if (link.attr('href') == window.location.href.replace(re, '')) {
+            item.addClass('active');
+        }
+    });
+});
+
+function randCommandBtnColors() {
+    $('.command-btn').each(function(idx, el) {
+        var randColor = function() { return Math.floor(Math.random(idx) * 256) },
+            hue = 'rgb(' + [randColor(), randColor(), randColor()].join(',') + ')';
+
+        $(el).css('background-color', hue);
+    });
+}
